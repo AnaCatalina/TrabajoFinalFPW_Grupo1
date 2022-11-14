d0c2eaad96d1a309ab9c817e792eec698e2361e5
fab1997 <93455997+fab1997@users.noreply.github.com>
2022-11-14 01:12:21 -0300

    comentarios del juego tateti

--- src/componente/Ta-Te-Ti.js
+++ src/componente/Ta-Te-Ti.js
@@ -8,11 +8,11 @@ import Puntaje from "./Ta-Te-ti/Puntaje";
 
 
 
-    
+    // agregamos sonido a cada click de los botones
   function play(){
       new Audio(sonido1).play();
   }
-
+// instacionamos un array que van a ser de las lineas posibles para ganar
 const combinacionGanadora = [
   [0, 1, 2],
   [3, 4, 5],
@@ -24,57 +24,58 @@ const combinacionGanadora = [
   [2, 4, 6],
 ];
 
+// en la funcion instanciamos que valores que nos va a devolver mediante el uso de los hooks
   const Game = () =>{
-    const [turno, setTurno] = useState("X")
-    const[lineaGanadora,setLineaGanadora] = useState([])
-    const[cuadrados, setCuadrados] = useState(Array(9).fill(null))
-    const[punto, setPunto]=useState({
+    const [turno, setTurno] = useState("X") // instanciamos el valor o el signo que comenzara el juego, en este caso el X
+    const[lineaGanadora,setLineaGanadora] = useState([])  // asignamos una variable que nos va a devolver un array, en este caso de las lineas ganadoras
+    const[cuadrados, setCuadrados] = useState(Array(9).fill(null)) // instanciamos un elemento cuadrado al cual le mandaremos un array de 9 elemento y que seran vacios
+    const[punto, setPunto]=useState({ // instanciamos los valores puntos que inicializan en 0
       X:0,
       O:0,
     });
-    const resetear = ()=>{
-      setTurno("X");
-      setCuadrados(Array(9).fill(null));
-      setLineaGanadora([]);
+    const resetear = ()=>{ // funcion encargafa de resetear el juego una vez que algun jugador logre ganar
+      setTurno("X"); // inicia el turno para las X
+      setCuadrados(Array(9).fill(null)); // los cuadrados vuelven a nulos
+      setLineaGanadora([]); // se reinician el array de las lineas haciendo desaparecer las X y O
     }
-    const checkWin = newCuadrado =>{
+    const checkWin = newCuadrado =>{ // funcion se encargada de analizar quien gano y mostrar las animaciones del tablero
       for(let i = 0; i < combinacionGanadora.length; i++){
         const[a,b,c] = combinacionGanadora[i];
-        if(newCuadrado[a] && newCuadrado[a] === newCuadrado[b] && newCuadrado[a] === newCuadrado[c]){
+        if(newCuadrado[a] && newCuadrado[a] === newCuadrado[b] && newCuadrado[a] === newCuadrado[c]){ // condicion para analizar si los cuadros seleccionados coinciden con el mismo signo
         //hay ganador
-        finJuego(newCuadrado[a],combinacionGanadora[i]);
+        finJuego(newCuadrado[a],combinacionGanadora[i]); // le pasamos el valor gandor 
         console.log('gano');
-        return
+        return // retornamos para que la funcion analice de nuevo el proximo movimiento
         }
       }
-      if(!newCuadrado.includes(null)){
+      if(!newCuadrado.includes(null)){ // si ninguno coincide 3 veces se genera un empate
         // hay empate
-        finJuego(null, Array.from(Array(10).keys()));
+        finJuego(null, Array.from(Array(10).keys())); // creamos un array de 10 enlementos para animar el tablero
         return
       }
       setTurno(turno === 'X' ? 'O' : 'X');
     }
 
-    const clikear = cuadrado => {
-      let newCuadrado = [...cuadrados];
+    const clikear = cuadrado => { // metodo usado para no dejar clikear otros cuadrados una vez que encuentre un gandor
+      let newCuadrado = [...cuadrados]; // desestructuramos los cuadrados 
       newCuadrado.splice(cuadrado, 1 , turno);
       setCuadrados(newCuadrado);
       checkWin(newCuadrado);
 
     }
-    const finJuego = (resultado, combinacionGanadora) =>{
+    const finJuego = (resultado, combinacionGanadora) =>{ // metodo utilizado para sumarle un punto a cada jugaodr
       
       setTurno(null);
       if(resultado !== null){
         setPunto({
-          ...punto,
+          ...punto, // desestructuramos el valor de puntos para poder sumarlos
           [resultado]:punto[resultado] + 1,
         })
       }
       setLineaGanadora(combinacionGanadora);
       setTimeout(() => {
         resetear();
-      }, 3000);
+      }, 3000); // con esto se resetea automaticamente los signos al cabo de 3 segundos
       
     }
     return (
--- src/componente/Ta-Te-ti/Cuadrado.js
+++ src/componente/Ta-Te-ti/Cuadrado.js
@@ -4,10 +4,10 @@ import "../Ta-Te-ti/style/Cuadrado.css";
 
 const Cuadrado = ({value, onClick, turno,gano})=>{
 
-    const apoyarClikear = () =>{
+    const apoyarClikear = () =>{ // metodo para analizar el turno dependiendo el valor que le corresponde
       (turno !== null && value === null) && onClick();
     }
-    let claseCuadrado = classNames({
+    let claseCuadrado = classNames({// logica para agregarle las clases a cada cuadrado, para poder darle la forma de los simbolos
         cuadrado:true,
         [`cuadrado--${value}`]: value !==null,
         gano:gano,
